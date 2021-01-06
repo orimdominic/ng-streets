93516928e40391ff0c0a8e92c7ace6663515b1c7
Orim Dominic <orimdominic@gmail.com>
2021-01-06 13:11:18 +0100

    feat: getState, getStates

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+"use strict";
+export { getStates, getState } from "./ng-streets";
---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export {IState} from './IState'
+export {IArea} from './IArea'
+export {IStreet} from './IStreet'
+export {default as pipe} from './pipe'
