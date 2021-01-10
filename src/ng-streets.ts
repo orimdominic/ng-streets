2e3005dc2fc92b8373ee5427a1cebd19d434f74a
Orim Dominic <orimdominic@gmail.com>
2021-01-10 17:35:28 +0100

    fix: #3

--- src/ng-streets.ts
+++ src/ng-streets.ts
@@ -18,37 +18,4 @@ const getState = (name: string): IState | undefined => {
   return data.find((state) => state.name.toLowerCase() === name.toLowerCase());
 };
 
-// /**
-//  * Returns the data (name, streets) of every area in a state
-//  * @param {string}  stateName - the name of the state
-//  * @returns {Array<IArea>} an array containing the name of each area in `stateName`
-//  */
-// const getAreas = (stateName: string): Array<IArea> | undefined => {
-//   const state = data.find(
-//     (state) => state.name.toLowerCase() === stateName.toLowerCase()
-//   );
-//   if (state === undefined) return undefined;
-//   return state.areas.map((area) => area);
-// };
-
-// /**
-//  * Returns data of an area in a state
-//  * @param {string}  stateName - the name of the state
-//  * @returns {Array<IArea>} an array containing the name of each area in `stateName`
-//  */
-// const getArea = (areaName: string): IArea | undefined => {
-//   return undefined
-// };
-
-// /**
-//  * Returns the name of every street in an area
-//  * @param {string}  area - the name of the state
-//  * @returns {Array<string>} an array containing the name of each street in `area`
-//  */
-// const getStreets = (area: string): Array<string> | undefined => {
-//   const state = data.find((state) => state.name === stateName);
-//   if (state === undefined) return undefined;
-//   return state.areas.map((area) => area["name"]);
-// };
-
 export { getStates, getState };
